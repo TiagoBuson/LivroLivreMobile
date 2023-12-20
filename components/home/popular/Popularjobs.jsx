9ec817efba518ea6df72cb63464668b92ef4bc96
TiagoBuson <tiagobuson@gmail.com>
2023-12-20 17:24:38 -0300

    first commit

---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
+
+const Popularjobs = () => {
+  const router = useRouter();
+
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
+
+  const livros = [
+    {
+      nome: "Harry Potter",
+      numPags: 320,
+      cod: 123,
+      job_id: 1234
+    },
+    {
+      nome: "Percy Jackson",
+      numPags: 450,
+      cod: 124,
+      job_id: 1235
+    },
+    {
+      nome: "Narnia",
+      numPags: 1200,
+      cod: 125,
+      job_id: 1236
+    }
+]
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Livros Populares</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Mostrar tudo</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (
+          <Text>Algo deu errado</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
+            keyExtractor={(item) => item.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs
---
+++ constants/images.js
@@ -0,0 +1,6 @@
+import profile from "../assets/images/kemal.jpg";
+import logoUnb from "../assets/images/logoUnb.png";
+
+export default {
+  profile, logoUnb
+};
